555455fb9d755926f3ffc8b4ed70c8cc5f9fda35
Khadija Khalid <khadijakhalid128@gmail.com>
2025-10-24 21:49:47 +0100

    updated files

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+// function updateTime() {
+//   const timeElement = document.getElementById("time");
+//   timeElement.textContent =Time.now();
+// }
+
+// updateTime();
+
+// setInterval(updateTime, 1000);
+
 function updateTime() {
   const timeElement = document.getElementById("time");
-  timeElement.textContent = Date.now();
+  const now = new Date();
+  const readable = now.toLocaleString();
+  const ms = now.getTime();
+  timeElement.textContent = `Current Time: ${readable} (${ms})`;
 }
 
 updateTime();
-
 setInterval(updateTime, 1000);
